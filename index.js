9d299d5eadc64ef589a1fdbebe78b3d757619157
sondos alansi <sondos.alansi@yahoo.com>
2018-10-18 17:37:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+var customerName = 'bob';
+const leastFavoriteCustomer = 'sour fred';
 
 function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+ customerName = customerName.toUpperCase();
+   return customerName;
 }
 
 function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
+  leastFavoriteCustomer = 'sour fred';
 }
 
 
 function setBestCustomer(){
-  bestCustomer = 'not bob'
+  bestCustomer = 'not bob';
 }
 
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
 
 function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+    let  favoriteCustomer = 'bar';
+    let  favoriteCustomer = 'not bar';
 }
